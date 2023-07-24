c668efb1ae52e12a32e1bfdd87fe12686b2fe191
세희 <seahee0130@gmail.com>
2023-07-24 17:47:32 +0900

    [D2] Title: 달팽이 숫자, Time: 132 ms, Memory: 21,376 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -3,45 +3,34 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-			Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+		// 방향(우 > 하 > 좌 > 상)
+		int dr[] = { 0, 1, 0, -1 };
+		int dc[] = { 1, 0, -1, 0 };
 
-		for (int cnt = 1 ; cnt < T+1; cnt++) {
+		for (int cnt = 1; cnt < T + 1; cnt++) {
 			int N = sc.nextInt();
 			int[][] ans = new int[N][N]; // N*N 크기의 배열 생성
 			int r = 0; // 행 index
 			int c = 0; // 열 index
+			int num = 1; //
+			int idx = 0; // 방향 조건
 
-			ans[r][c] = 1; // 시작점은 (0,0) = 1
-			int num = 2; // 배열에 들어갈 숫자 (1씩 증가)
-			// 종료시점은 num == N * N
-			// 방향(우 > 하 > 좌 > 상)
 			while (num <= N * N) {
-				// 우
-				while (c + 1 < N && ans[r][c + 1] == 0) {
-					c++;
-					ans[r][c] = num;
-					num++;
+				ans[r][c] = num++;
+				int nr = r + dr[idx];
+				int nc = c + dc[idx];
+				// 배열 경계를 벗어나거나 이미 숫자가 들어가 있으면 방향 전환
+				if (nr < 0 || nr >= N || nc < 0 || nc >= N || ans[nr][nc] != 0) {
+					idx = (idx + 1) % 4;
+					nr = r + dr[idx];
+					nc = c + dc[idx];
 				}
-				// 하
-				while (r + 1 < N && ans[r + 1][c] == 0) {
-					r++;
-					ans[r][c] = num;
-					num++;
-				}
-				// 좌
-				while (c - 1 >= 0 && ans[r][c - 1] == 0) {
-					c--;
-					ans[r][c] = num;
-					num++;
-				}
-				// 상
-				while (r - 1 >= 0 && ans[r - 1][c] == 0) {
-					r--;
-					ans[r][c] = num;
-					num++;
-				}
+				r = nr;
+				c = nc;
 			}
+
 			System.out.println("#" + cnt);
 			for (int i = 0; i < N; i++) {
 				for (int n : ans[i])
