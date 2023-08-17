91ab0d91e79fc35eb83f26dbf6b7d2c0f5eea1c7
세희 <seahee0130@gmail.com>
2023-08-17 14:37:46 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기1, Time: 134 ms, Memory: 21,464 KB -BaekjoonHub

--- SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
+++ SWEA/D4/1222. ［S／W 문제해결 기본］ 6일차 － 계산기1/［S／W 문제해결 기본］ 6일차 － 계산기1.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Solution {
 	// + 연산자 하나라 우선순위 모두 동일, 앃이는 경우가 없음
 	public static char[] stack;
+	public static int[] stackN;
 	public static int top;
 
 	public static void main(String[] args) {
@@ -15,7 +16,7 @@ public class Solution {
 
 			// 1. 중위 -> 후위로 변경
 			top = -1;
-			stack = new char[2];
+			stack = new char[N];
 			for (int i = 0; i < N; i++) {
 				char tmp = infix.charAt(i); // 중위 표기법 한글자씩 읽어오기
 				if (tmp - '0' >= 0 && tmp - '0' <= 9) // 읽어온 문자가 피연산자라면
@@ -36,21 +37,21 @@ public class Solution {
 
 			// 2. 후위표기법 >> 계산
 			top = -1;
-			stack = new char[2];
+			stackN = new int[N];
 			for (int i = 0; i < N; i++) {
 				char tmp = postfix.charAt(i);
 				if (tmp - '0' >= 0 && tmp - '0' <= 9) // 읽어온 문자가 피연산자라면
-					push(tmp); // push
+					stackN[++top] = tmp - '0'; // push
 				else if (tmp == '+') { // 연산자라면
 					// 순서 중요 (a + b)
-					int b = pop() - '0'; // 늦게 들어간게 b
-					int a = pop() - '0'; // 일찍 들어간게 a
-					char sum = (char) (a + b + '0'); // 연산 후 char 변환
-					push(sum); // 연산결과 다시 push
+					int b = stackN[top--]; // 늦게 들어간게 b
+					int a = stackN[top--]; // 일찍 들어간게 a
+					int sum = a + b; // 연산 후 char 변환
+					stackN[++top] = sum; // 연산결과 다시 push
 				}
 			}
 			// 끝났으면 pop해서 결과 출력
-			int ans = pop() - '0';
+			int ans = stackN[top--];
 
 			System.out.println("#" + tc + " " + ans);
 		} // 테스트 케이스만큼 반복
