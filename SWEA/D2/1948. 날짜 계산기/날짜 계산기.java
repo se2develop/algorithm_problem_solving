ab16a5acdb7f15763a6632e0267e159f3232538e
세희 <seahee0130@gmail.com>
2023-08-18 15:55:45 +0900

    [D2] Title: 날짜 계산기, Time: 130 ms, Memory: 20,348 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.java
@@ -1,44 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int[] date = new int[12];
-		for (int i = 0; i < 12; i++) {
-			if (i == 1) {
-				date[i] = 28;
-			} else if (i == 3 || i == 5 || i == 8 || i == 10) {
-				date[i] = 30;
-			} else
-				date[i] = 31;
-		} // 각 달의 마지막 날짜 배열에 저장
+		// 각 월의 마지막 날짜 저장하는 배열
+		int[] date = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 		int T = sc.nextInt(); // 테스트 케이스 개수
 		for (int tc = 1; tc <= T; tc++) {
-			int ans = 0; // 두 번째 날짜가 첫 번째 날짜의 며칠째인지
+			int sMonth = sc.nextInt(); // 첫 번째 날짜 -> 월
+			int sDay = sc.nextInt(); // 첫 번째 날짜 -> 일
+			int eMonth = sc.nextInt(); // 두 번째 날짜 -> 월
+			int eDay = sc.nextInt(); // 두 번째 날짜 -> 일
 
-			int sMonth = sc.nextInt(); // 첫 번째 날짜 > 월
-			int sDay = sc.nextInt(); // 첫 번째 날짜 > 일
-			int eMonth = sc.nextInt(); // 두 번째 날짜 > 월
-			int eDay = sc.nextInt(); // 두 번째 날짜 > 일
+			int ans = 0; // 정답 -> 두 번째 날짜가 첫 번째 날짜의 며칠째인지!
 
-			// 같은 달이면 일끼리만 빼면 되고
 			if (sMonth == eMonth) {
 				ans = eDay - sDay + 1;
-			}
-			// 다른 달이면
+			} // 두 날짜의 월이 같다면 일 수만 빼주면 됨!
 			else {
-				// 두 번째 날짜의 일 더하고
-				ans += eDay;
-				// 두 날짜 사이에 있는 달의 모든 일수를 더하고
-				for (int i = eMonth - 2; i >= sMonth; i--) {
+				// 1) 첫 번째 날짜로부터 다음달까지 남은 일 수 계산
+				ans += date[sMonth - 1] - sDay + 1;
+				// 2) 두 달 사이의 일 수 계산
+				for (int i = sMonth; i <= eMonth - 2; i++) {
 					ans += date[i];
 				}
-				// 첫 번째 월의 마지막 날짜에서 - 첫 번째 일 + 1
-				ans += date[sMonth - 1] - sDay + 1;
+				// 3) 두 번째 날짜의 일 수 계산
+				ans += eDay;
 			}
 			System.out.println("#" + tc + " " + ans);
-		}
-	}
+		} // 테스트 케이스만큼 반복
+
+	} 
 }
+
