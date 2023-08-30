7cf866759c5bd17f0fcb2d63ee0154e88a3d551c
세희 <seahee0130@gmail.com>
2023-08-30 23:22:47 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 25732 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	// 중복 조합
+	public static int N;
+	public static int R;
+	public static int[] arr;
+	public static int[] sel;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt(); // 전체 개수
+		R = sc.nextInt(); // 뽑을 개수
+
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		sel = new int[R];
+
+		dcomb(0, 0);
+		System.out.println(sb);
+	} // main
+
+	public static void dcomb(int idx, int sidx) {
+		if (sidx == R) {
+			for (int i = 0; i < R; i++) {
+				sb.append(sel[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = idx; i < arr.length; i++) {
+			sel[sidx] = arr[i];
+			dcomb(i, sidx + 1);
+		}
+	} // dcomb
+} // class
