4ec5239a2ef2bfd243638c6ab65b1267f1d7eaec
세희 <seahee0130@gmail.com>
2023-08-31 00:20:51 +0900

    [Silver II] Title: N과 M (11), Time: 588 ms, Memory: 243808 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	// 중복 순열
+	public static int N;
+	public static int R;
+	public static int[] arr;
+	public static boolean[] sel;
+	public static int[] ans;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt(); // 전체 개수
+		R = sc.nextInt(); // 뽑을 개수
+
+		arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+
+		sel = new boolean[N];
+		ans = new int[R];
+
+		dperm(0);
+		System.out.println(sb);
+
+	} // main
+
+	public static void dperm(int idx) {
+		if (idx == R) {
+			for (int i = 0; i < R; i++) {
+				sb.append(ans[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		int tmp = -1; // 중복 확인해주는 변수
+		for (int i = 0; i < N; i++) {
+			if (tmp == arr[i])
+				continue;
+			ans[idx] = arr[i];
+			dperm(idx + 1);
+			tmp = arr[i];
+		}
+	} // dperm
+} // class
