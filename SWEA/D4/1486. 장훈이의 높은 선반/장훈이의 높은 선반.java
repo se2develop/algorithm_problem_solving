187a5b523c575ae107e015255e52494c83e77e2a
세희 <seahee0130@gmail.com>
2023-09-04 09:37:47 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 138 ms, Memory: 21,072 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Solution {
 	public static int N, B, ans;
 	public static int[] H;
-	public static boolean[] sel;
 
 	public static void main(String[] args) {
 
@@ -17,36 +16,32 @@ public class Solution {
 
 			// 모든 부분집합을 구해서 판단해보자
 			H = new int[N];
-			sel = new boolean[N]; // 해당 점원의 포함 여부
+
 			for (int i = 0; i < N; i++) {
 				H[i] = sc.nextInt();
 			}
 
 			ans = 987654321;
-			powerset(0);
+			powerset(0, 0);
 			System.out.println("#" + tc + " " + (ans - B));
 
 		} // tc for문
 
 	} // main
 
-	public static void powerset(int idx) {
+	// sum = 중간합
+	public static void powerset(int idx, int sum) {
 		// 기저 부분
 		if (idx == N) {
-			int sum = 0;
-			for (int i = 0; i < N; i++) {
-				if (sel[i])
-					sum += H[i];
-			}
-			if (sum >= B && sum < ans)
-				ans = sum;
+			if (sum >= B)
+				ans = Math.min(ans, sum);
 			return; // 종료조건 !!
 		}
+
 		// 재귀 부분
-		sel[idx] = false; // 미포함
-		powerset(idx + 1);
-		sel[idx] = true; // 포함
-		powerset(idx + 1);
+		// 중간합을 사용 > 어떤 점원이 포함되었는지는 알 수 없음
+		powerset(idx + 1, sum + H[idx]);
+		powerset(idx + 1, sum);
 	}
 
 } // class
