dd90b2f6bffcf900b5c8f4d9ff11b52e5e20d373
세희 <seahee0130@gmail.com>
2023-09-04 12:37:49 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 198 ms, Memory: 21,884 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -1,54 +1,51 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static int N, B, ans, rs;
+	public static int N, B, ans;
 	public static int[] H;
+	public static boolean[] visited;
 
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
 
 		int T = sc.nextInt(); // 테스트 케이스 개수
 
 		for (int tc = 1; tc <= T; tc++) {
 			N = sc.nextInt(); // 점원의 수
-			B = sc.nextInt(); // 높이가 B이상인 탑
+			B = sc.nextInt(); // 탑의 최소 높이
 
-			// 모든 부분집합을 구해서 판단해보자
 			H = new int[N];
+			visited = new boolean[N];
 
+			ans = 987654321; // 높이가 B이상인 탑 중 가장 최소 높이
 			for (int i = 0; i < N; i++) {
 				H[i] = sc.nextInt();
-				rs += H[i];
-			}
+			} // 입력받기 끝!
 
-			ans = 987654321;
-			powerset(0, 0, rs);
+			subset(0);
 			System.out.println("#" + tc + " " + (ans - B));
-
 		} // tc for문
 
 	} // main
 
-	// sum = 중간합
-	// rsum = 남은 키의 합
-	public static void powerset(int idx, int sum, int rsum) {
-		// 내가 가지고 있는 ans보다 크면 탈출!
-		if (sum > ans)
-			return;
-		// 기저 부분
+	public static void subset(int idx) {
 		if (idx == N) {
-			if (sum >= B)
-				ans = Math.min(ans, sum);
-			return; // 종료조건 !!
-		}
-		// sum + rsum(중간합 + 남은 키의 합) < B(최소 탑의 높이)이면 조건 만족 X >> 탈출
-		if (sum + rsum < B)
+			int tmp = 0;
+			for (int i = 0; i < N; i++) {
+				// visited[i]가 true인 직원들의 높이를 tmp에 저장
+				if (visited[i])
+					tmp += H[i];
+			}
+			// tmp가 B이상이고, ans보다 작으면 갱신
+			if (tmp >= B && tmp < ans)
+				ans = tmp;
 			return;
+		}
 
-		// 재귀 부분
-		powerset(idx + 1, sum + H[idx], rsum - H[idx]);
-		powerset(idx + 1, sum, rsum - H[idx]);
-	}
-
+		// 부분집합 구하기
+		visited[idx] = true;
+		subset(idx + 1);
+		visited[idx] = false;
+		subset(idx + 1);
+	} // subset
 } // class
