c0bae13ee2cd1cc6bd5383e76ede6525d84b2a6b
세희 <seahee0130@gmail.com>
2023-09-18 16:20:04 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 3,462 ms, Memory: 23,764 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,87 +1,79 @@
 import java.util.Scanner;
 
 public class Solution {
-	public static int cntWin; // 규영이가 이기는 경우의 수
-	public static int cntLose; // 규영이가 지는 경우의 수
-	public static int scoreK, scoreI;
-	public static int[] kyu, in;
-	public static boolean[] card;
-	public static int[] result;
-	public static boolean[] visited;
+	public static int[] gyu, in, tmp;
+	public static boolean[] flag, visited;
+	public static int gscore, iscore, gwin, glose;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int T = sc.nextInt(); // 테스트 케이스 개수
+		int T = sc.nextInt(); // 테스트케이스 개수
+		gyu = new int[9]; // 규영이가 갖는 수
+		in = new int[9]; // 인영이가 갖는 수
+		visited = new boolean[9];
+		tmp = new int[9]; // 각 순열을 저장할 배열
 
 		for (int tc = 1; tc <= T; tc++) {
-			card = new boolean[18]; // 규영이 선택 정보 저장
+			flag = new boolean[19]; // 규영이가 갖은 수의 정보 저장해서 인영이에게 주려고!
+			gwin = 0; // 규영이가 이기는 횟수
+			glose = 0; // 규영이가 지는 횟수
 
-			kyu = new int[9]; // 규영이가 가진 카드(순서 고정)
 			for (int i = 0; i < 9; i++) {
-				kyu[i] = sc.nextInt();
-				card[kyu[i] - 1] = true; // 규영이가 선택한 카드면 true
+				gyu[i] = sc.nextInt(); // 규영이 카드와 내는 순서를 받으며
+				flag[gyu[i]] = true; // 받은 카드 정보를 체크
 			}
 
-			in = new int[9]; // 인영이가 가진 카드(순서 고정X)
+			// 인영이는 규영이가 갖고있지 않는 수
 			int idx = 0;
-			for (int i = 0; i < 18; i++) {
-				if (!card[i]) {
-					in[idx++] = i + 1;
+			for (int i = 1; i <= 18; i++) {
+				if (flag[i] == false) { // 규영이가 받지 않은 카드면
+					in[idx++] = i; // 인영이에게!
 				}
 			}
 
-			result = new int[9]; // 인영이가 카드를 내는 경우의 수
-			visited = new boolean[9];
-			cntWin = 0; // 규영이가 이긴 횟수
-			cntLose = 0; // 규영이가 진 횟수
+			perm(0); // 계산해보자!
+			System.out.println("#" + tc + " " + gwin + " " + glose);
 
-			perm(0);
-
-			System.out.println("#" + tc + " " + cntWin + " " + cntLose);
 		} // tc for문
-
 	} // main
 
-	public static void perm(int idx) {
-		if (idx == 9) {
-			// 인영이가 내는 카드 순서에 따라 승패 계산
-			cal(kyu, result);
-			return;
+	public static void perm(int cnt) {
+		// 여기서 규영이와 인영이 카드 비교
+		if (cnt == 9) {
+			// 각 경우 둘의 점수는 0에서 시작
+			gscore = 0;
+			iscore = 0;
+			// 9장의 카드를 비교
+			for (int i = 0; i < 9; i++) {
+				// 규영이 카드가 크면 규영이가 점수얻고
+
+				if (gyu[i] > tmp[i])
+					gscore += gyu[i] + tmp[i];
+				// 인영이 카드가 크면 인영이가 점수얻고
+				else
+					iscore += gyu[i] + tmp[i];
+			}
+
+			// 규영이의 점수가 크면
+			if (gscore > iscore)
+				gwin++; // 이긴횟수++
+			// 규영이의 점수가 작으면
+			else if (gscore < iscore)
+				glose++; // 진횟수++
 		}
 
+		// 사용할 수 있는 모든 원소를 체크!
 		for (int i = 0; i < 9; i++) {
-			if (visited[i]) // 이미 사용한 원소면 건너뜀
+			if (visited[i]) // 이미 사용했으면 건너뜀
 				continue;
 
-			result[idx] = in[i]; // 해당 i번째 원소를 저장
-			visited[i] = true; // i번재 원소 사용했다고 표시
-			perm(idx + 1);
+			tmp[cnt] = in[i]; // i번째 원소 저장
+			visited[i] = true; // 사용여부 표시
+			perm(cnt + 1);
 			visited[i] = false; // 사용여부 원상복구
 		}
+
 	} // perm
-
-	public static void cal(int[] arr1, int[] arr2) {
-		scoreK = 0;
-		scoreI = 0;
-		for (int i = 0; i < 9; i++) {
-			// 규영이가 이기면
-			int score = arr1[i] + arr2[i]; // 이긴사람이 획득한 점수
-			if (arr1[i] - arr2[i] > 0) {
-				scoreK += score;
-			}
-			// 인영이가 이기면
-			else {
-				scoreI += score;
-			}
-		}
-
-		// 규영이 총점이 더 높으면 규영이가 이긴 횟수 + 1
-		if (scoreK > scoreI)
-			cntWin++;
-		else if (scoreK < scoreI)
-			cntLose++;
-
-	} // cal
 
 } // class
