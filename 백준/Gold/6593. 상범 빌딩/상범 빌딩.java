675857ea08325460329c99089ebad006b248a9df
세희 <seahee0130@gmail.com>
2023-09-19 17:35:00 +0900

    [Gold V] Title: 상범 빌딩, Time: 248 ms, Memory: 19896 KB -BaekjoonHub

--- 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
+++ 백준/Gold/6593. 상범 빌딩/상범 빌딩.java
@@ -3,9 +3,9 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	public static int L, R, C, time;
+	public static int L, R, C, ans;
 	public static char[][][] building;
-	public static int[][][] visited;
+	public static int[][][] time;
 	public static int sfloor, srow, scol, efloor, erow, ecol; // 시작점 좌표, 출구 좌표 저장
 	// 상하좌우남북
 	public static int[] dr = { -1, 1, 0, 0, 0, 0 };
@@ -15,15 +15,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		// 무한반복
 		while (true) {
 			L = sc.nextInt(); // 상범 빌딩의 층 수
 			R = sc.nextInt(); // 한 층의 행의 개수
 			C = sc.nextInt(); // 한 층의 열의 개수
+
+			// L,R,C가 모두 0이면 강제종료
 			if (L == 0 && R == 0 && C == 0)
 				System.exit(0);
 
-			building = new char[L][R][C];
-			visited = new int[L][R][C];
+			building = new char[L][R][C]; // 상범빌딩 정보
+			time = new int[L][R][C]; // 각 층별, 칸별 방문 정보
 
 			for (int f = 0; f < L; f++) {
 				for (int i = 0; i < R; i++) {
@@ -45,38 +48,47 @@ public class Main {
 				sc.nextLine();
 			} // 층 -> 입력받기 끝
 
-			time = 0;
-			BFS(sfloor, srow, scol);
-			if (time == 0) {
+			ans = 0; // 탐색 횟수라고 생각하면 됨
+			BFS(sfloor, srow, scol); // BFS는 최단거리
+
+			// time이 0이면 도달 X
+			if (ans == 0) {
 				System.out.println("Trapped!");
 			} else {
-				System.out.println("Escaped in " + (time - 1) + " minute(s).");
+				System.out.println("Escaped in " + ans + " minute(s).");
 			}
 		} // (0,0,0) 들어오면 종료
 	} // main
 
 	public static void BFS(int f, int r, int c) {
 		Queue<int[]> queue = new LinkedList<>();
+		// 큐에 시작점 좌표 넣고 방문에 걸린 시간 저장(시작점은 0)
 		queue.add(new int[] { f, r, c });
-		visited[f][r][c] = 1;
+		time[f][r][c] = 0;
 
 		while (!queue.isEmpty()) {
 			int[] now = queue.poll();
 
+			// 도착점을 만나면
 			if (building[now[0]][now[1]][now[2]] == 'E') {
-				time = visited[now[0]][now[1]][now[2]];
+				// 도착점을 올 때까지 탐색횟수 저장하고 종료
+				ans = time[now[0]][now[1]][now[2]];
 				return;
 			}
 
+			// 상,하,좌,우,남,북 탐색
 			for (int idx = 0; idx < 6; idx++) {
 				int nf = now[0] + df[idx];
 				int nr = now[1] + dr[idx];
 				int nc = now[2] + dc[idx];
 
+				// 경계를 벗어나지 않으면서
 				if (nf >= 0 && nf < L && nr >= 0 && nr < R && nc >= 0 && nc < C) {
-					if ((building[nf][nr][nc] == '.' || building[nf][nr][nc] == 'E') && visited[nf][nr][nc] == 0) {
+					// 갈 수 있는 길(.)이나 도착점이면서 아직 방문하지 않았으면
+					if ((building[nf][nr][nc] == '.' || building[nf][nr][nc] == 'E') && time[nf][nr][nc] == 0) {
+						// 큐에 추가하고 방문에 걸린 시간 저장
 						queue.add(new int[] { nf, nr, nc });
-						visited[nf][nr][nc] = visited[now[0]][now[1]][now[2]] + 1;
+						time[nf][nr][nc] = time[now[0]][now[1]][now[2]] + 1;
 					}
 				}
 			}
